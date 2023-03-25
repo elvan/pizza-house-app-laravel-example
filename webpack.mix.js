ad34a7d398b407fad5335adbb3bfed2fbd84fc3a
Elvan Hidayat <elvanhd@gmail.com>
2023-03-25 17:37:55 +0700

    laravel auth setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/main.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js').sass(
+    'resources/sass/app.scss',
+    'public/css'
+);
